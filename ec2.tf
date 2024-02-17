805b578b33d537815518a6214e64e45453dee425
manthan <mpmanthan07@gmail.com>
2024-02-17 10:01:38 +0000

    pepline

---
+++ ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "webserver_access" {
+        name = "webserver_access"
+        description = "allow ssh and http"
+
+        ingress {
+                from_port = 80
+                to_port = 80
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        ingress {
+                from_port = 22
+                to_port = 22
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+                from_port = 0
+                to_port = 0
+                protocol = "-1"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+
+}
+
+resource "aws_instance" "ourfirst" {
+  ami           = "ami-03f4878755434977f"
+  availability_zone = "ap-south-1a"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.webserver_access.name}"]
+  tags = {
+    Name  = "ec2 with security"
+  }
+
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIATIP3WBBILLRY4ZLP"
+  secret_key = "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"
+}
